2a60910217708bd94b11f79e8a0b480c55ddd176
Roshan Gupta <rgroshanrg@gmail.com>
2021-04-22 17:00:57 +0530

    done

--- config/keys.js
+++ config/keys.js
@@ -1,18 +1,18 @@
-// module.exports = {
-//     MONGO : {
-//         URI : 'mongodb+srv://greenweb:T2LuJUA5TDVzxxF@greenworld.nuza1.mongodb.net/greendb?retryWrites=true&w=majority'
-//     },
-//     GMAIL : {
-//         EMAIL_ID : 'greenworlds2021@gmail.com',
-//         EMAIL_PASS : 'GREEN_worlds2021',
-//         EMAIL_SECRET : 'RhxNcQsspLC3jS8EXwB4WudBtom_jerry!23yodhatherealwarriors@gmail.comtom_jerry!23'
-//     },
-//     GOOGLE : {
-//         CLIENT_ID : '112111093085-4srl9tgjjb35cn1jpcpmn65afu7i49tg.apps.googleusercontent.com',
-//         CLIENT_SECRET : 'faFp1a2ilAc2IzeUW2dCaAJX'
-//     },
-//     SESSION : {
-//         COOKIE_KEY : 'tom_jerry!23yodhatherealwarriors@gmail.comtom_jerry!23'
-//     },
-//     baseUrl :  'https://green-world-plant.herokuapp.com/'
-// } 
+module.exports = {
+    MONGO : {
+        URI : 'YOUR-MONGO-URI'
+    },
+    GMAIL : {
+        EMAIL_ID : 'YOUR-EMAIL-ID',
+        EMAIL_PASS : 'YOUR-EMAIL-PASSWORD',
+        EMAIL_SECRET : 'EMAIL-SECRET'   //RANDOM
+    },
+    GOOGLE : {
+        CLIENT_ID : 'GOOGLE-0AUTH-CLIENT-ID',
+        CLIENT_SECRET : 'GOOGLE-0AUTH-CLIENT-SECRET'
+    },
+    SESSION : {
+        COOKIE_KEY : 'COOKIE-KEY'   //RANDOM
+    },
+    baseUrl :  'BASE-URL'
+} 
